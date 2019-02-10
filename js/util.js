173f3e0a9f89ac5dda3775cc349638ad57006216
Unknown <jackagriffiths@gmail.com>
2019-02-10 14:46:47 -0800

    Auto stash before merge of "master" and "origin/master"

--- js/util.js
+++ js/util.js
@@ -2,3 +2,10 @@ export const dateConvert = (time) => {
     var newTime = time.toLocaleString()
     console.log(newTime)
 }
+
+export function geoConv(address) {
+    var str = address
+    var res = str.replace(/ /g, "+");
+    console.log(res)
+}
+
