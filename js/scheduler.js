10f6105df4ddc85ef864d97c5005421d2c9cf405
Unknown <jackagriffiths@gmail.com>
2019-02-10 02:08:24 -0800

    Fleshing out functions

--- js/scheduler.js
+++ js/scheduler.js
@@ -4,23 +4,41 @@
 // } for time objects
 
 function addSchedule(patient, clinic, doctor, time){
-    // get schedule for clinic
 
+    // get schedule for clinic from doctor
+    if (!clinic.doctors.includes(doctor)) 
+    {
+        return console.error("Doctor does not exist in this clinic!\n")
+    }
+
+    let doctorSchedule = clinic.doctors[doctor].schedule // an array of objects, sorted
     // see if conflicts
+    let sTime = time.sTime
+    let fTime = time.fTime
 
+    if (sTime >= fTime) 
+    {
+        return console.error("Start time cannot be later than finish time!\n")
+    }
     // add if not, return error if conflict
+    let prevSTime = 0
+    
+
     // create schedule object with time, patient name etc, add to doctors schedule, sort 
 }
 
-function removeSchedule(clinic, doctor, time){
+function removeSchedule(clinic, doctor, time)
+{
     // removes schedule object that is in that spot
 }
 
-function editSchedule(clinic, doctor, newSchedule){
+function editSchedule(clinic, doctor, newSchedule)
+{
 
 }
 
-function checkConflicts(clinic, doctor, time){
+function checkConflicts(clinic, doctor, time)
+{
     // check that doctor exists for that clinic
 
     // access the  doctors schedule for the day
