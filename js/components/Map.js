61b9d84e47e2dcdb3f4cf13074665a9200690742
Jocelyn <jocelyntytang@gmail.com>
2019-02-10 14:11:25 -0800

    map popup-edit

--- js/components/Map.js
+++ js/components/Map.js
@@ -75,17 +75,18 @@ export const initGoogleMaps = () => {
 		var markerConnect = new google.maps.Marker({position: ConnLoc, icon: clinicMarker, map: map});
 		var markerWELL = new google.maps.Marker({position: WELLLoc, icon: clinicMarker, map: map});
 		
-		var infoWindowZen = new google.maps.InfoWindow({content: `<strong>Dr. Danny Devita</strong>
+		var infoWindowZen = new google.maps.InfoWindow({content: `<div><strong>Dr. Danny Devita</strong>
 		Gender: Male
-		Hours: 11:00am-6:00pm
+		Hours: 11:00am-6:00pm </div>
+		<div><strong>Dr. Donkey Kong</strong>
+		Gender: Male
+		Hours: 3:00pm-6:00pm </div>
 		<button type="button">Click Me!</button>`})
 		markerZen.addListener('click', function(){
 			infoWindowZen.open(map, markerZen)
 		})
 		
-		var infoWindowBaysw = new google.maps.InfoWindow({content: `<strong>Dr. Donkey Kong</strong>
-		Gender: Male
-		Hours: 3:00pm-6:00pm <button type="button">Click Me!</button>`})
+		var infoWindowBaysw = new google.maps.InfoWindow({content: `<button type="button">Click Me!</button>`})
 		markerBaysw.addListener('click', function(){
 			infoWindowBaysw.open(map, markerBaysw)
 		})
